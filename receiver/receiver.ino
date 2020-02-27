57c6fcffe623c08af49719984b140772d4d1b131
ProstoProgrammer <58485325+yarikyarikYar@users.noreply.github.com>
2020-02-27 09:29:32 +0500

    receiver update

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -6,9 +6,24 @@
 Servo servo;
 RF24 radio(9,10);
 int data[2];
+int led;
 
 void setup() {
+  pinMode(led,OUTPUT);
   radio.begin();
+  radio.setChannel(5);
   radio.setDataRate(RF24_1MBPS);
   radio.PALevel(RF24_PA_HIGH);
+  radio.openReadingPipe(1,0x1234567890LL)
+  radio.startListening(); 
+}
+
+void loop() {
+  if(radio.available) {
+    digitalWrite(led,HIGh);
+    radio.read(&data,sizeof(data))
+    servo.write(data[0])
+  }else{
+    digitalWrite(led,LOW);
+  }
 }
